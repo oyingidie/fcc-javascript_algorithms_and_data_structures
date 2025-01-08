a1ba5b615d20c8d818df469dddb691159fd2c743
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-08 20:01:17 +0100

    Anna Ban@na

--- 0x00-palindrome_checker/script.js
+++ 0x00-palindrome_checker/script.js
@@ -2,3 +2,12 @@ const textInput = document.getElementById("text-input");
 const checkButton = document.getElementById("check-btn");
 const result = document.getElementById("result");
 
+const validateUserInput = () => {
+  if (!textInput.value.trim()) {
+    result.classList.add("hide");
+    alert("Please input a value.");
+  } else {
+    result.innerHTML = `<p>Ooops! Wanna try again?</p>`;
+    result.classList.remove("hide");
+  }
+};
