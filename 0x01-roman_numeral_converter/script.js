ed4f0af2469915791de4611f72d6d54988fe3e1d
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-09 03:31:54 +0100

    Festina Lente

--- 0x01-roman_numeral_converter/script.js
+++ 0x01-roman_numeral_converter/script.js
@@ -9,15 +9,15 @@ const convertInput = (num) => {
     M: 1000,
     CM: 900,
     D: 500,
-    // CD: 400,
+    CD: 400,
     C: 100,
     XC: 90,
     L: 50,
-    // XL: 40,
+    XL: 40,
     X: 10,
     IX: 9,
     V: 5,
-    // IV: 4,
+    IV: 4,
     I: 1
   };
 
