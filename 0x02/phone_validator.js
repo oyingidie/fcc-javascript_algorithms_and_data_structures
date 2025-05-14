8041204fa2e05e228a14d242a926278f0c32fd9e
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-05-14 21:06:02 +0100

    W A R N I N G: Spoof Alert!

--- 0x02/phone_validator.js
+++ 0x02/phone_validator.js
@@ -2,3 +2,6 @@ const userInput = document.getElementById("user-input");
 const checkButton = document.getElementById("check-btn");
 const clearButton = document.getElementById("clear-btn");
 const results = document.getElementById("results-div");
+
+
+const validateNumber = () => {};
