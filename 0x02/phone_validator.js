5ed2efdbef056f63bffe957c1779d41a0c9e54be
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-05-14 21:07:13 +0100

    W A R N I N G: Spoof Alert!

--- 0x02/phone_validator.js
+++ 0x02/phone_validator.js
@@ -3,5 +3,11 @@ const checkButton = document.getElementById("check-btn");
 const clearButton = document.getElementById("clear-btn");
 const results = document.getElementById("results-div");
 
+const validateNumber = () => {
+  const phoneNumber = userInput.value;
 
-const validateNumber = () => {};
+  if (!phoneNumber) {
+    alert("Please provide a phone number");
+    return;
+  }
+};
