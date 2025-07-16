dc355a09c66f877c7e99d07f2ef42ed0a90c06bb
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-07-16 07:49:56 +0100

    Cha-ching!

--- 0x03/cash_register.js
+++ 0x03/cash_register.js
@@ -5,3 +5,16 @@ const priceDisplay = document.getElementById("price-display");
 const cashInDrawerDisplay = document.getElementById("cid-display");
 const confirmChangeDialog = document.getElementById("confirm-change-dialog");
 const completeBtn = document.getElementById("complete-btn");
+
+let price = 1.87;
+let cid = [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100]
+];
