bc43cd2bd13eaa8981169b21e4db483a9e917217
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-07-16 07:42:09 +0100

    Cha-ching!

--- 0x03/cash_register.js
+++ 0x03/cash_register.js
@@ -1,1 +1,7 @@
-
+const cashInput = document.getElementById("cash");
+const purchaseBtn = document.getElementById("purchase-btn");
+const change = document.getElementById("change-due");
+const priceDisplay = document.getElementById("price-display");
+const cashInDrawerDisplay = document.getElementById("cid-display");
+const confirmChangeDialog = document.getElementById("confirm-change-dialog");
+const completeBtn = document.getElementById("complete-btn");
