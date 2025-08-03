7bcbdf98a678e8159925fb89852c4f6018ede00b
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-08-03 21:19:48 +0100

    The retro gamer's favourite drink

--- 0x04/rpg_creature.js
+++ 0x04/rpg_creature.js
@@ -1,1 +1,14 @@
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+const creatureNameElement = document.getElementById("creature-name");
+const creatureIdElement = document.getElementById("creature-id");
+const weightElement = document.getElementById("weight");
+const heightElement = document.getElementById("height");
+const typesElement = document.getElementById("types");
+const hpElement = document.getElementById("hp");
+const attackElement = document.getElementById("attack");
+const defenseElement = document.getElementById("defense");
+const specialAttackElement = document.getElementById("special-attack");
+const specialDefenseElement = document.getElementById("special-defense");
+const speedElement = document.getElementById("speed");
 
